08ced6d93c3a0e506db4c2339f152002d3f98c63
Zeiadork <zeiagillies@gmail.com>
2022-11-28 22:18:50 +0000

    add rules ref

--- src/components/UnitForm/index.js
+++ src/components/UnitForm/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './style.css'
 
 export const UnitForm = () => {
+
+  const [name,setName] = useState('');
+  const [description,setDescription] = useState('');
+  const [points,setPoints] = useState('');
+  const [quality,setQuality] = useState('');
+  const [defense,setDefense] = useState('');
+  const [weapons,setWeapons] = useState('');
+  const [rules,setRules] = useState('')
+
+
 
   return(
     <>
---
+++ src/specialRules/specialRules.js
@@ -0,0 +1,21 @@
+//This is going to be a pain to code. Maybe do a switch statement on calc property instead, have calc be an integer that refers to a cost ruleset....  Will keep this for easier reference at least.
+const specialRules = [
+    {name: 'Aircraft', cost: 15, calc: 0},
+    {name: 'Ambush', cost: 'quality', calc: 1},
+    {name: 'Fast', cost: 'quality', calc: 1},
+    {name: 'Fear', cost: 20, calc: 0},
+    {name: 'Fearless', cost: 'baseCostQuality+1', calc: 3},
+    {name: 'Flying', cost: 'quality', calc: 1},
+    {name: 'Furious',cost: 'mostExpensiveMeleeWeapon/2', calc: 4},
+    {name: 'Hero', cost: 0, calc: 0},
+    {name: 'Immobile', cost: 'quality*-3', calc: 5},
+    {name: 'Impact(X)', cost: 'X * 3', calc: 6},
+    {name: 'Psychic(X)', cost: 'X * 15 + 5', calc: 7},
+    {name: 'Regeneration', cost: 'tough * 6', calc: 8},
+    {name: 'Relentless', cost: 'costOfRangedWeapons * 1.125', calc: 9},
+    {name: 'Scout', cost: 'quality', calc: 1},
+    {name: 'Slow', cost: '- quality', calc: 10},
+    {name: 'Stealth', cost: 'Tough(X) * 2', calc: 11},
+    {name: 'Strider', cost: 'Quality / 2', calc: 12},
+    {name: 'Transport(X)', cost: 'X * 2', calc: 13}
+]
