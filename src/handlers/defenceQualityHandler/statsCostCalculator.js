039b593f88344c887b5588ee8c7e339169144fbd
Zeiadork <zeiagillies@gmail.com>
2022-11-29 19:39:07 +0000

    work on fleshing out handlers for points calcs

---
+++ src/handlers/defenceQualityHandler/statsCostCalculator.js
@@ -0,0 +1,28 @@
+const statsCostCalculator = (stat) => {
+
+    let cost = 0;
+
+    switch (stat) {
+        case 6:
+            cost = cost + 2
+            break;
+        case 5:
+            cost = cost + 4
+            break;
+        case 4:
+            cost = cost + 6
+            break;
+        case 3:
+            cost = cost + 8
+            break;
+        case 2:
+            cost = cost + 12
+            break
+        default:
+            console.warn(`Invalid quality/defense, ${stat}`)
+    }
+
+    return cost;
+}
+
+exports = module.exports = { statsCostCalculator }
--- src/handlers/pointCalculator.js
+++ src/handlers/pointCalculator.js
@@ -1,43 +1,50 @@
+const { statsCostCalculator } = require('./defenceQualityHandler/statsCostCalculator')
+const { weaponCostCalculator } = require('./')
+
 const pointCalculator = (unit) => {
 
-    const stats = getStatsCost(unit.stats);
-    const weapons = getWeaponsCost(unit.weapons);
-    const rules = getRulesCost(unit.rules);
+    const stats = getStatsCost(unit);
+    const weapons = getWeaponsCost(unit);
+    const rules = getRulesCost(unit);
 
-    const total = stats + weapons + rules
+    const modelTotal = stats + weapons + rules
 
-    return [total, stats, weapons, rules]
+    const unitTotal = modelTotal * unit.models
+
+
+    return [unitTotal, modelTotal, stats, weapons, rules]
 
 }
 
-const getStatsCost = (stats) => {
+const getStatsCost = (unit) => {
+
+    return statsCostCalculator(unit.quality) + statsCostCalculator(unit.defense);
+
+}
+
+const getWeaponsCost = (unit) => {
+
+    let cost = 0;
+
+    for (weapon in unit.weapons) {
+        cost = cost + weaponCostCalculator(weapon)
+    }
+
+    return cost;
+
+}
+
+const getRulesCost = (unit) => {
 
     let cost = 0;
 
     //TODO CALC COST
 
     return cost;
-}
-
-const getWeaponsCost = (weapons) => {
-
-    let cost = 0;
-
-    //TODO CALC COST
-
-    return cost;
 
 }
 
-const getRulesCost = (rules) => {
 
-    let cost = 0;
-
-    //TODO CALC COST
-
-    return cost;
-
-}
 
 
 exports = module.exports = { pointCalculator }
